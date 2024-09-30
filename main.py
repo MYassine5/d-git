8c1887ecb54e384ae01963cd88748ecc411a33ed
Moulay Yassine <sida.moulay@gmail.com>
2024-09-30 14:48:13 +0100

    again

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-<<<<<<< Updated upstream
-=======
-#i would like 
-#me too i would
->>>>>>> Stashed changes
-def main():
+def m():
     print("ah ana ma kan chouf main branch ana aaah machi rajl ana")
     print("lherba")
     print("spv")
