be270090586ce66a100f576b82cedee1c9d55ebb
Moulay Yassine <sida.moulay@gmail.com>
2024-09-30 15:33:34 +0100

    L

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 def main():
     print("ah ana ma kan chouf main branch ana aaah machi rajl ana")
     print("lherba")
+    a = 2
     print("mchat ghir hakak")
     print("spv")
     print("an  nadi")
